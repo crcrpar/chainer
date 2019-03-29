1a97b4208f92c907ea17d16ad514ae0ceaf8ba07
crcrpar <masaki.kozuki.2014@gmail.com>
2019-03-30 00:19:50 +0900

    make normalization directory

--- chainer/link_hooks/__init__.py
+++ chainer/link_hooks/__init__.py
@@ -1,3 +1,4 @@
-from chainer.link_hooks.spectral_normalization import SpectralNormalization  # NOQA
 from chainer.link_hooks.timer import TimerHook  # NOQA
 from chainer.link_hooks.weight_normalization_base import WeightNormalizationBase  # NOQA
+
+from chainer.link_hooks.normalization.spectral_normalization import SpectralNormalization  # NOQA
